f60972dd1659083149e96c0d4d3116b1dba082a9
Robert <rjwolke@gmail.com>
2022-11-23 21:36:33 +0100

    reduced calls to database by caching lock data added method for saving the testbench list to json

--- TACo.py
+++ TACo.py
@@ -1,32 +1,51 @@
-from datetime import datetime
 import os
-from typing import List, Tuple
+import json
+from typing import Dict, List, Tuple
+from datetime import datetime
+from pathlib import Path
+
+import psutil
+
 from taco.DatabaseController import DatabaseController
 from taco.Testbench import Testbench
 
 
 class TestbenchAccessController():
     # SETTINGS_FILE = os.path.join(os.environ['LOCALAPPDATA'], '.taco')
-    SETTINGS_FILE = '.taco_settings'
+    SETTINGS_FILE       = '.taco_settings'  # Settings file
+    LOCK_UPDATE_TIMER   = 10                # Time after which lock data is refreshed
     
+
     def __init__(self):
+        self.database: DatabaseController = None
+
         self.username: str = os.getlogin()      # Default Username
-        self.testbenchJson: str = ''
+       
+        self.testbenchJson: Path = Path()
         self.testbenches: List[Testbench] = []
         self.tb_structure = []
-        self.databaseController: DatabaseController = None
+        
+        self.lock_cache: Dict[str, Tuple[str, datetime]] = {}
+        self.lock_cache_time: datetime = datetime.min
+        
+        self.subprocesses: Dict[int, int] = {}
         
         self.load_settings()
         
     
     @property
-    def databaseFile(self) -> str:
+    def databaseFile(self) -> Path:
         try:
-            return self.databaseController.dbFile
+            return Path(self.database.dbFile)
         except AttributeError:
-            return ''
+            return Path()
         
-    
+        
+    @property            
+    def lock_cache_age(self) -> int:
+        return (datetime.now() - self.lock_cache_time).total_seconds()
+
+
     def set_username(self, username) -> None:
         if username:
             self.username = username
@@ -35,14 +54,14 @@ class TestbenchAccessController():
         self.save_settings()
 
 
-    def set_database(self, database, create = False) -> Tuple[bool, str]:
+    def set_database(self, database) -> Tuple[bool, str]:
         if not database:
             return (False, 'No database selected')
         
-        self.databaseController = DatabaseController(database)
+        self.database = DatabaseController(database)
         for tb in self.testbenches:
             try:
-                self.get_lock(tb.name)
+                self.database.add_testbench(tb.hostname)
             except ValueError as err:
                 return (False, err)
 
@@ -50,69 +69,132 @@ class TestbenchAccessController():
         return (True, '')
     
 
-    def load_testbench_JSON(self, testbenchJson: str) -> None:
-        self.testbenchJson = os.path.abspath(testbenchJson)
+    def load_testbench_JSON(self, testbenchJson: str) -> bool:
+        self.testbenchJson = Path(testbenchJson)
         self.testbenches = []
         self.tb_structure = []
         try:
             with open(testbenchJson, 'r') as f:
-                json = f.read()
+                testbenchdata = json.load(f)
         except (FileNotFoundError, PermissionError):
-            json = "{}"
+            testbenchdata = {}
         
-        for testbench_list in eval(json):
+        for testbench_list in testbenchdata:
             self.tb_structure.append({})
             for hostname, data in testbench_list.items():
                 self.add_testbench(hostname, data)
 
         self.save_settings()
-           
-                
-    def add_testbench(self, name: str, data: dict, list_index: int = -1, isChild: bool = False) -> None:
-        hostname    = data.get('hostname', '')
-        login_name  = data.get('login_name', '')
-        self.testbenches.append(Testbench(name, hostname, login_name))
+        return bool(testbenchdata)
 
-        if self.databaseController is not None:
-            self.databaseController.add_testbench(name)
+
+    def save_testbench_JSON(self, testbenchJson: str) -> bool:
+        def serialize_testbench(id: str, children: List[str] = None) -> Dict[str,str]:
+            data = {}
+            tb = self.get_testbench(id)
+            if tb.hostname != tb.id:
+                data['hostname'] = tb.hostname
+            if tb.login_name: 
+                data['login_name'] = tb.login_name
+            if children:
+                data['children'] = {child: serialize_testbench(child) for child in children}
+            return data
+        
+        self.testbenchJson = Path(testbenchJson)
+
+        testbenchdata = []
+        for tb_block in self.tb_structure:
+            testbench_list = {}
+            for id, children in tb_block.items():
+                testbench_list[id] = serialize_testbench(id, children)
+            testbenchdata.append(testbench_list)
+
+        try:
+            with open(testbenchJson, 'w') as f:
+                json.dump(testbenchdata, f, indent=4)
+        except (PermissionError):
+            return False
+        
+        return True
+        
+                
+    def add_testbench(self, id: str, data: dict, isChild: bool = False) -> None:
+        hostname    = data.get('hostname', id)
+        login_name  = data.get('login_name', '')
+        self.testbenches.append(Testbench(id, hostname, login_name))
+        self.lock_cache[id] = ('', datetime.now())
+
+        if self.database is not None:
+            self.database.add_testbench(hostname)
 
         if not isChild:
             children    = data.get('children', {})
-            self.tb_structure[list_index][name] = children.keys()
+            self.tb_structure[-1][id] = children.keys()
             for childname, childdata in children.items():
-                self.add_testbench(childname, childdata, list_index, isChild = True)
+                self.add_testbench(childname, childdata, isChild = True)
             
 
-    def get_testbench_by_name(self, name: str) -> Testbench:
+    def get_testbench(self, id: str) -> Testbench:
         try:
-            return next(testbench for testbench in self.testbenches if testbench.name == name)
+            return next(testbench for testbench in self.testbenches if testbench.id == id)
         except StopIteration as err:
             raise ValueError(err)
-    
+        
+        
+    def update_locks(self) -> None:
+        self.lock_cache_time = datetime.now()
+        if self.database is None:
+            return
+        
+        self.unlock_by_pid()
+        
+        hostnames = tuple(tb.hostname for tb in self.testbenches)
+        lockDict = self.database.get_lock_multiple(hostnames)
+        self.lock_cache.update(lockDict)
 
-    def get_lock(self, name: str) -> Tuple[str, datetime]:
-        if self.databaseController is None:
+            
+    def get_lock(self, id: str, forceRefresh: bool = False) -> Tuple[str, datetime]:
+        if self.database is None:
             return ('', datetime.now())
         
-        try:
-            return self.databaseController.get_lock(name)
-        except ValueError:
-            self.databaseController.add_testbench(name)
-            return ('', datetime.now())
+        if not forceRefresh and self.lock_cache_age >= self.LOCK_UPDATE_TIMER:
+            self.update_locks()
+        
+        hostname = self.get_testbench(id).hostname
+        return self.lock_cache[hostname]
  
     
-    def set_lock(self, name: str) -> None:
-        self.databaseController.set_lock(name, self.username)
+    def __set_lock(self, id: str, username: str) -> None:
+        hostname = self.get_testbench(id).hostname
+        self.lock_cache[hostname] = (username, datetime.now())
+        self.database.set_lock(hostname, username)
+        
+
+    def set_lock(self, id: str) -> None:
+        return self.__set_lock(id, self.username)
 
     
-    def unset_lock(self, name: str) -> None:
-        self.databaseController.set_lock(name, '')
+    def unset_lock(self, id: str) -> None:
+        return self.__set_lock(id, '')
+
+    
+    def run_rdp(self, id: str) -> None:
+        self.set_lock(id)
+        pid = self.get_testbench(id).run_rdp()
+        self.subprocesses[id] = pid
+
+
+    def unlock_by_pid(self):
+        for id, pid in self.subprocesses.items():
+            if self.lock_cache[id][0] == self.username:
+                if not pid in psutil.pids():
+                    self.unset_lock(id)
 
 
     def load_settings(self) -> None:
         try:
             with open(self.SETTINGS_FILE, 'r') as f:
-                settings = eval(f.read())
+                settings = json.load(f)
         except (IOError, SyntaxError):
             self.save_settings()    # Create new Settings File
             settings = {}
@@ -125,11 +207,8 @@ class TestbenchAccessController():
     def save_settings(self) -> None:
         settings = {}
         settings['Username']        = self.username
-        settings['Testbenchfile']   = self.testbenchJson
-        try:
-            settings['Database']    = self.databaseFile
-        except AttributeError:
-            settings['Database']    = ''
+        settings['Testbenchfile']   = str(self.testbenchJson.absolute())
+        settings['Database']        = str(self.databaseFile.absolute())
         
         with open(self.SETTINGS_FILE, 'w') as f:
-            f.write(str(settings))
+            json.dump(settings, f, indent=4)
--- taco/DatabaseController.py
+++ taco/DatabaseController.py
@@ -1,11 +1,16 @@
 import os
 import sqlite3
 from datetime import datetime
-from typing import Tuple
+from typing import Dict, List, Tuple
 
 
 class DatabaseController():
     def __init__(self, databaseFile : str) -> None:
+        """Initializes the sqlite connection and creates the required table.
+
+        Args:
+            databaseFile (str): Path to the database file.
+        """
         self.dbFile     = os.path.abspath(databaseFile)
         self.connection = sqlite3.connect(self.dbFile, 
                                           detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
@@ -14,25 +19,36 @@ class DatabaseController():
         self.create_testbench_table(False)
         
         
-    def create_testbench_table(self, forceRecreate = False) -> None:
+    def create_testbench_table(self, forceRecreate: bool = False) -> None:
+        """Creates the table "Testbenches".
+
+        Args:
+            forceRecreate (bool, optional): Drops the table before creating it. Defaults to False.
+        """
+        
         table = """CREATE TABLE IF NOT EXISTS Testbenches (
             Name VARCHAR(255) NOT NULL UNIQUE,
             Locked_By CHAR(255),
             Locked_Since TIMESTAMP)
         """
         if forceRecreate:
-            try:
-                self.cursor.execute("DROP TABLE Testbenches")
-            except sqlite3.OperationalError:
-                pass
+            self.cursor.execute("DROP TABLE IF EXISTS Testbenches")
             
         self.cursor.execute(table)
         self.connection.commit()
         
         
-    def add_testbench(self, name) -> None:
+    def add_testbench(self, hostname: str) -> None:
+        """Adds the specified testbench to the table "Testbenches" in the database. The lock data is empty with a timestamp of now().
+
+        Args:
+            hostname (str): Hostname of the testbench.
+
+        Raises:
+            ValueError: Raised if the table "Testbenches" deviates from the required format.
+        """
         try:
-            self.cursor.execute("INSERT INTO Testbenches VALUES (?, '', ?)", (name, datetime.now()))
+            self.cursor.execute("INSERT INTO Testbenches VALUES (?, '', ?)", (hostname, datetime.now()))
         except sqlite3.IntegrityError:
             # Testbench already exists
             return
@@ -43,16 +59,65 @@ class DatabaseController():
         self.connection.commit()
         
 
-    def get_lock(self, name: str) -> Tuple[str, datetime]:
-        self.cursor.execute("SELECT Locked_By, Locked_Since FROM Testbenches WHERE Name IS ?", (name,))
+    def get_lock(self, hostname: str) -> Tuple[str, datetime]:
+        """_summary_
+
+        Args:
+            hostname (str): hostname of the computer
+
+        Raises:
+            ValueError: Raised if the specified hostname is not found in the database
+
+        Returns:
+            Tuple[str, datetime]: tuple of locked_by, locked_since
+        """
+        self.cursor.execute("SELECT Locked_By, Locked_Since FROM Testbenches WHERE Name IS ?", (hostname,))
+        
         lockData = self.cursor.fetchone()
         if not lockData:
-            raise ValueError(f'Testbench "{name}" not found in database "{self.dbFile}"')
+            raise ValueError(f'Testbench "{hostname}" not found in database "{self.dbFile}".')
         
         return lockData
     
     
-    def set_lock(self, name: str, lockedBy: str) -> None:
-        self.cursor.execute("UPDATE Testbenches SET Locked_By = ?, Locked_Since = ? WHERE Name IS ?", (lockedBy, datetime.now(), name))
+    def get_lock_multiple(self, hostnames: Tuple[str] = ()) -> Dict[str, Tuple[str, datetime]]:
+        """Get Lock Data for multiple entries in the database.
+
+        Args:
+            hostnames (List[str], optional): List of hostnames. Defaults to [], meaning all hosts found in the database.
+
+        Raises:
+            ValueError: Raised if any specified hostname is not found in the database
+
+        Returns:
+            dict[str, Tuple[str, datetime]]: dictionary linking hostnames to a tuple of locked_by, locked_since
+        """
+        
+        query = "SELECT Name, Locked_By, Locked_Since FROM Testbenches"
+        if hostnames:
+            query += " WHERE Name IN ({0})".format(", ".join('?' for _ in hostnames))
+
+        self.cursor.execute(query, hostnames)
+        lockData = self.cursor.fetchall()
+        
+        lockDict = {}
+        for hostname, lock_user, lock_time in lockData:
+            lockDict[hostname] = (lock_user, lock_time)
+        
+        missingdata = set(hostnames) - set(lockDict.keys())
+        if missingdata:
+            raise ValueError(f'Testbench(es) "{list(missingdata)}" not found in database "{self.dbFile}".')
+       
+        return lockDict
+    
+    
+    def set_lock(self, hostname: str, lock_user: str) -> None:
+        """Sets a lock in the database for the specified host.
+
+        Args:
+            hostname (str): hostname of the computer
+            lockedBy (str): name of the user assigned to the lock
+        """
+        self.cursor.execute("UPDATE Testbenches SET Locked_By = ?, Locked_Since = ? WHERE Name IS ?", (lock_user, datetime.now(), hostname))
         self.connection.commit()
         
