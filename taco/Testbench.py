0d4f5fa2a054b5ed1e4b58f030f53aa14fc4566c
Robert <rjwolke@gmail.com>
2022-11-23 21:34:37 +0100

    ip address only on demand changed "name" to "id"

--- taco/Testbench.py
+++ taco/Testbench.py
@@ -1,40 +1,43 @@
 import subprocess as sp
 import os
 import socket
-from typing import List
+from typing import Dict
+from datetime import datetime
 
 
 class Testbench():
-    def __init__(self, name: str, hostname: str = '', login_name: str = '') -> None:
-        self.name       = name
-        self.hostname   = hostname if hostname else name
+    def __init__(self, id: str, hostname: str = '', login_name: str = '') -> None:
+        self.id         = id
+        self.hostname   = hostname if hostname else id
         self.login_name = login_name
-        
-        # try:
-        #     self.ip_address = socket.gethostbyname(self.hostname)
-        # except socket.gaierror:
-        #     self.ip_address = ''
-        self.ip_address = '127.0.0.1'
-        
+   
         
     def __repr__(self) -> str:
-        return f'{self.name} ({self.hostname})'
-       
-
-    def run_rdp(self) -> None:       
-        rdp_file = self.create_rdp_file()   # Create RDP file
-        sp.Popen(['mstsc.exe', rdp_file])   # Open Remote Desktop Connection without waiting for a return
+        return f'{self.id} ({self.hostname})'
     
+
+    def run_rdp(self) -> int:       
+        rdp_file = self.create_rdp_file()   # Create RDP file
+        process = sp.Popen(['mstsc.exe', rdp_file])   # Open Remote Desktop Connection without waiting for a return
+        return process.pid
+    
+    
+    def get_ip_address(self) -> str:
+        try:
+            return socket.gethostbyname(self.hostname)
+        except socket.gaierror:
+            return ''
     
     def create_rdp_file(self) -> str:
         rdpSettings = []
-        rdpSettings.append(f'full address:s:{self.ip_address}\n')   # Target Computer
-        rdpSettings.append(f'username:s:{self.login_name}\n')       # Login Name
-        rdpSettings.append('screen mode id:i:2\n')                  # Full Screen
-        rdpSettings.append('use multimon:i:0\n')                    # Single Monitor
+        rdpSettings.append(f'full address:s:{self.get_ip_address()}\n') # Target Computer
+        rdpSettings.append(f'username:s:{self.login_name}\n')           # Login Name
+        rdpSettings.append('screen mode id:i:2\n')                      # Full Screen
+        rdpSettings.append('use multimon:i:0\n')                        # Single Monitor
         
-        path = os.path.join(os.environ['TEMP'], f'RDP_{self.name}.rdp')
+        path = os.path.join(os.environ['TEMP'], f'RDP_{self.id}.rdp')
         with open(path, 'w') as rdpfile:
             rdpfile.writelines(rdpSettings)
             
         return path
+    
